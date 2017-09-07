ce3c8e5b5725e8a4da2e43d4a85594f04bd929e7
Manuelgn27 <31710101+Manuelgn27@users.noreply.github.com>
2017-09-07 08:31:47 -0600

    Add files via upload

---
+++ Semana1/codigo.php
@@ -0,0 +1,12 @@
+<?php /* variables.php */
+echo "<h1>Varibles. PHP no tiene tipos.</h1>"
+$nombre="Ricardo";
+$edad=14;
+echo "<h3>Me llamo $nombre y tengo $edad años</h3>";
+echo "<h2>Si uso comilla simple, no se leen variables</h2>";
+echo '<h3>Me llamo $nombre y tengo $edad años</h3>';
+
+$edadMeses=$edad*12;
+echo "<p>Edad en meses: $edadMeses</p>"
+
+?>
